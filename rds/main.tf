9d0bdb58d31dd23f18c3b35f81e8e8416cdddc1c
Cheuk Lau <cheuk.lau@aggienetwork.com>
2020-03-15 22:57:00 -0700

    Add backend to rds module

--- rds/main.tf
+++ rds/main.tf
@@ -1,3 +1,13 @@
+provider "aws" {
+  region  = "us-west-2"
+  version = "~> 2.25.0"
+}
+
+terraform {
+  backend "s3" {}
+  required_version = ">=0.12.3"
+}
+
 resource "aws_db_instance" "mtgtradingpostdb" {
   allocated_storage    = 20
   storage_type         = "gp2"
