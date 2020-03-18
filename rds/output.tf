b21c7b30f7bf1f10bce2aca18af7fcec8d280bb4
Cheuk Lau <cheuk.lau@aggienetwork.com>
2020-03-17 21:55:32 -0700

    Remove Route53 output from rds module

--- rds/output.tf
+++ rds/output.tf
@@ -7,8 +7,3 @@ output "endpoint" {
     value = "${aws_db_instance.mtgtradingpostdb.endpoint}"
     description = "The connection endpoint."
 }
-
-output "fqdn" {
-    value = "${aws_route53_record.mtgtradingpostdb.fqdn}"
-    description = "Primary DNS name based on the custom domain name."
-}
