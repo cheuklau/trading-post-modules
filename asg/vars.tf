aaa9a97dfa8e183681a96f43a5424d44555f5c84
Cheuk Lau <cheuk.lau@aggienetwork.com>
2020-03-19 23:48:17 -0700

    Add asg module

---
+++ asg/vars.tf
@@ -0,0 +1,24 @@
+variable "instance_type" {
+  description = "EC2 instance type to deploy"
+  type        = string
+}
+
+variable "min_size" {
+  description = "Minimum number of EC2 Instances to run in the ASG"
+  type        = number
+}
+
+variable "max_size" {
+  description = "Maximum number of EC2 Instances to run in the ASG"
+  type        = number
+}
+
+variable "server_port" {
+  description = "Port number web server on each EC2 Instance should listen on for HTTP requests"
+  type        = number
+}
+
+variable "elb_port" {
+  description = "Port number ELB should listen on for HTTP requests"
+  type        = number
+}
